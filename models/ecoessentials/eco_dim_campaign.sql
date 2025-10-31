6cb01bfe4ee9706d2ffad60d334bd012f7d9ccec
Ryan Seal <A02324672@usu.edu>
2025-10-31 21:52:51 +0000

    updated changes 10/31 spoooooooky

--- models/ecoessentials/eco_dim_campaign.sql
+++ models/ecoessentials/eco_dim_campaign.sql
@@ -8,6 +8,7 @@
 
 WITH promotional AS (
     SELECT
+        CAMPAIGN_ID AS campaign_key,
         CAMPAIGN_NAME,
         CAMPAIGN_DISCOUNT,
         NULL AS PROMOTIONAL_CAMPAIGN
@@ -16,6 +17,7 @@ WITH promotional AS (
 
 orderline AS (
     SELECT
+        NULL AS campaign_key,
         NULL AS CAMPAIGN_NAME,
         NULL AS CAMPAIGN_DISCOUNT,
         PROMOTIONAL_CAMPAIGN
@@ -25,6 +27,7 @@ orderline AS (
 SELECT * FROM promotional
 UNION ALL
 SELECT * FROM orderline
+
 
 --There is also a campaign name in the salesforce email source data, not sure if we need that/want to include it
 --Also not sure if this is connected to the rest of the data warehouse, because there is no source linked on the lineage graph below.
--- models/ecoessentials/eco_dim_date.sql
+++ models/ecoessentials/eco_dim_date.sql
@@ -2,15 +2,14 @@
     materialized = 'table',
     database = 'GROUP1PROJECT',
     schema = 'dw_ecoessentials'
-    )
-}}
-
+) }}
 
 WITH marketing AS (
     SELECT
         EVENTTIMESTAMP,
         SENDTIMESTAMP,
-        NULL AS ORDER_TIMESTAMP
+        NULL AS ORDER_TIMESTAMP,
+        NULL AS date_key
     FROM {{ source('salesforce_landing', 'marketing_emails') }}
 ),
 
@@ -18,13 +17,11 @@ orders AS (
     SELECT
         NULL AS EVENTTIMESTAMP,
         NULL AS SENDTIMESTAMP,
-        ORDER_TIMESTAMP
+        ORDER_TIMESTAMP AS ORDER_TIMESTAMP,
+        ORDER_TIMESTAMP AS date_key
     FROM {{ source('transactional_landing', 'orders') }}
 )
 
 SELECT * FROM marketing
 UNION ALL
 SELECT * FROM orders
-
---How can we create a date key/do we need one?
---Also not sure if this is connected to the rest of the data warehouse, because there is no source linked on the lineage graph below.
