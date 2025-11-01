6fd664a04f47e561c2c746a6e0221ac0cc0faa64
Ryan Seal <A02324672@usu.edu>
2025-11-01 20:11:57 +0000

    CHANGES 11/1

--- models/ecoessentials/eco_dim_customer.sql
+++ models/ecoessentials/eco_dim_customer.sql
@@ -5,7 +5,7 @@
     )
 }}
 
-
+WITH customer as (
 select
 customer_id as customer_key,
 customer_id,
@@ -16,9 +16,38 @@ customer_address,
 customer_city,
 customer_state,
 customer_zip,
-customer_country
+customer_country,
+NULL AS subscriberid,
+NULL AS customerid,
+NULL AS subscriberemail,
+NULL AS subscriberfirstname,
+NULL AS subscriberlastname
 FROM {{ source('transactional_landing', 'customer') }}
+),
 
+salesforce_email as (
+select
+NULL AS customer_key,
+NULL AS customer_id,
+NULL AS customer_first_name,
+NULL AS customer_last_name,
+NULL AS customer_phone,
+NULL AS customer_address,
+NULL AS customer_city,
+NULL AS customer_state,
+NULL AS customer_zip,
+NULL AS customer_country,
+subscriberid,
+customerid,
+subscriberemail,
+subscriberfirstname,
+subscriberlastname
+FROM {{ source('salesforce_landing', 'marketing_emails') }}
+)
+
+SELECT * FROM customer
+UNION ALL
+SELECT * FROM salesforce_email
 --We have this as a conformed dimension because we were doing to use it in conjuction with the salesforce email data.
 --The main fields that we were going to combine/use together were customer email, first name, last name, and subscriber email, first name, last name.
 --Not sure if these are the same or not but we should figure out if we need to have one, the other, or both.
--- models/ecoessentials/eco_fact_sales.sql
+++ models/ecoessentials/eco_fact_sales.sql
@@ -5,7 +5,7 @@
 ) }}
 
 SELECT
-    p.product_id,
+    ep.product_key,
     c.customer_key,
     d.date_key,
     cp.campaign_key,
@@ -16,6 +16,7 @@ SELECT
 FROM {{ source('transactional_landing', 'product') }} p
 INNER JOIN {{ source('transactional_landing', 'order_line') }} ol ON p.product_id = ol.product_id
 INNER JOIN {{ source('transactional_landing', 'orders') }} o ON ol.order_id = o.order_id
+INNER JOIN {{ ref('eco_dim_product') }} ep ON ol.product_id = ep.product_id 
 INNER JOIN {{ ref('eco_dim_customer') }} c ON o.customer_id = c.customer_id 
 INNER JOIN {{ ref('eco_dim_campaign') }} cp ON ol.campaign_id = cp.campaign_key 
 INNER JOIN {{ ref('eco_dim_date') }} d ON d.date_key = o.order_timestamp 
