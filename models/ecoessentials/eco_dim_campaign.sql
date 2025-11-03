34898a141cd7e38ff500ee175fb78afd31b632f0
Ryan Seal <A02324672@usu.edu>
2025-11-03 01:33:13 +0000

    changes 11/2 part 1

--- models/ecoessentials/eco_dim_campaign.sql
+++ models/ecoessentials/eco_dim_campaign.sql
@@ -2,9 +2,7 @@
     materialized = 'table',
     database = 'GROUP1PROJECT',
     schema = 'dw_ecoessentials'
-    )
-}}
-
+) }}
 
 WITH promotional AS (
     SELECT
@@ -17,17 +15,18 @@ WITH promotional AS (
 
 orderline AS (
     SELECT
-        NULL AS campaign_key,
+        CAMPAIGN_ID AS campaign_key,
         NULL AS CAMPAIGN_NAME,
         NULL AS CAMPAIGN_DISCOUNT,
         PROMOTIONAL_CAMPAIGN
     FROM {{ source('transactional_landing', 'order_line') }}
 )
 
-SELECT * FROM promotional
-UNION ALL
-SELECT * FROM orderline
-
-
---There is also a campaign name in the salesforce email source data, not sure if we need that/want to include it
---Also not sure if this is connected to the rest of the data warehouse, because there is no source linked on the lineage graph below.
+SELECT 
+    COALESCE(p.campaign_key, o.campaign_key) AS campaign_key,
+    p.CAMPAIGN_NAME,
+    p.CAMPAIGN_DISCOUNT,
+    o.PROMOTIONAL_CAMPAIGN
+FROM promotional p
+FULL JOIN orderline o
+    ON p.campaign_key = o.campaign_key
--- models/ecoessentials/eco_dim_customer.sql
+++ models/ecoessentials/eco_dim_customer.sql
@@ -2,53 +2,47 @@
     materialized = 'table',
     database = 'GROUP1PROJECT',
     schema = 'dw_ecoessentials'
-    )
-}}
+) }}
 
-WITH customer as (
-select
-customer_id as customer_key,
-customer_id,
-customer_first_name,
-customer_last_name,
-customer_phone,
-customer_address,
-customer_city,
-customer_state,
-customer_zip,
-customer_country,
-NULL AS subscriberid,
-NULL AS customerid,
-NULL AS subscriberemail,
-NULL AS subscriberfirstname,
-NULL AS subscriberlastname
-FROM {{ source('transactional_landing', 'customer') }}
+WITH customer AS (
+    SELECT
+        customer_id AS customer_key,
+        customer_id,
+        customer_first_name,
+        customer_last_name,
+        customer_phone,
+        customer_address,
+        customer_city,
+        customer_state,
+        customer_zip,
+        customer_country
+    FROM {{ source('transactional_landing', 'customer') }}
 ),
 
-salesforce_email as (
-select
-NULL AS customer_key,
-NULL AS customer_id,
-NULL AS customer_first_name,
-NULL AS customer_last_name,
-NULL AS customer_phone,
-NULL AS customer_address,
-NULL AS customer_city,
-NULL AS customer_state,
-NULL AS customer_zip,
-NULL AS customer_country,
-subscriberid,
-customerid,
-subscriberemail,
-subscriberfirstname,
-subscriberlastname
-FROM {{ source('salesforce_landing', 'marketing_emails') }}
+salesforce_email AS (
+    SELECT
+        customerid,
+        subscriberid,
+        subscriberemail,
+        subscriberfirstname,
+        subscriberlastname
+    FROM {{ source('salesforce_landing', 'marketing_emails') }}
 )
 
-SELECT * FROM customer
-UNION ALL
-SELECT * FROM salesforce_email
---We have this as a conformed dimension because we were doing to use it in conjuction with the salesforce email data.
---The main fields that we were going to combine/use together were customer email, first name, last name, and subscriber email, first name, last name.
---Not sure if these are the same or not but we should figure out if we need to have one, the other, or both.
---Also potentially need subscriber ID in here somewhere
+SELECT
+    c.customer_key,
+    c.customer_id,
+    COALESCE(c.customer_first_name, s.subscriberfirstname) AS customer_first_name,
+    COALESCE(c.customer_last_name, s.subscriberlastname) AS customer_last_name,
+    c.customer_phone,
+    c.customer_address,
+    c.customer_city,
+    c.customer_state,
+    c.customer_zip,
+    c.customer_country,
+    s.subscriberid,
+    s.subscriberemail
+FROM customer c
+FULL JOIN salesforce_email s
+    ON c.customer_first_name = s.subscriberfirstname
+    AND c.customer_last_name = s.subscriberlastname
