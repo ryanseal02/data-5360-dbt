87ec3dee710d4c4931559fa049704453a34d0698
Ryan Seal <A02324672@usu.edu>
2025-11-03 02:05:12 +0000

    changes pt 2

--- models/ecoessentials/eco_dim_campaign.sql
+++ models/ecoessentials/eco_dim_campaign.sql
@@ -8,25 +8,34 @@ WITH promotional AS (
     SELECT
         CAMPAIGN_ID AS campaign_key,
         CAMPAIGN_NAME,
-        CAMPAIGN_DISCOUNT,
-        NULL AS PROMOTIONAL_CAMPAIGN
+        CAMPAIGN_DISCOUNT
     FROM {{ source('transactional_landing', 'promotional_campaign') }}
 ),
 
 orderline AS (
     SELECT
         CAMPAIGN_ID AS campaign_key,
-        NULL AS CAMPAIGN_NAME,
-        NULL AS CAMPAIGN_DISCOUNT,
         PROMOTIONAL_CAMPAIGN
     FROM {{ source('transactional_landing', 'order_line') }}
+),
+
+salesforce AS (
+    SELECT
+        CAMPAIGNID,
+        CAMPAIGNNAME
+    FROM {{ source('salesforce_landing', 'marketing_emails') }}
 )
 
 SELECT 
-    COALESCE(p.campaign_key, o.campaign_key) AS campaign_key,
-    p.CAMPAIGN_NAME,
+    COALESCE(p.campaign_key, o.campaign_key, s.CAMPAIGNID) AS campaign_key,
+    p.CAMPAIGN_NAME AS transactional_campaign_name,
+    s.CAMPAIGNNAME AS salesforce_campaign_name,
     p.CAMPAIGN_DISCOUNT,
     o.PROMOTIONAL_CAMPAIGN
 FROM promotional p
 FULL JOIN orderline o
     ON p.campaign_key = o.campaign_key
+FULL JOIN salesforce s
+    ON COALESCE(p.campaign_key, o.campaign_key) = s.CAMPAIGNID
+
+
